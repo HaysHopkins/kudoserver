7ea040acb94cff7c1e45efdb7c8d1fc51c4d069e
HaysHopkins <whhopkinsiii@gmail.com>
2018-06-11 07:33:19 -0700

    Add generic api requester service

---
+++ client/src/components/utilities/apiRequesterService.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const ApiClient = window.origin;
+
+export const ApiRequester = {
+  get: (path, params) => (
+    axios.get(`/api/${path}`, params)
+      .then((resp) => {
+        return resp;
+      })
+  ),
+
+  put: (path, params) => (
+    axios.put(`${ApiClient}/api/${path}`, params)
+      .then((resp) => {
+        return resp;
+      })
+  ),
+
+  delete: (path, params) => (
+    axios.delete(`${ApiClient}/api/${path}`, params)
+      .then((resp) => {
+        return resp;
+      })
+  ),
+};
